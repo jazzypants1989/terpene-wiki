ed1eadccaa10088c71e540836403c2f97e3048ba
jazzypants1989 <jessepence@gmail.com>
2022-06-09 09:52:11 -0700

    why are things broken

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ async function apiRequest(){
         const response = await fetch(`https://terpene-api-demo.herokuapp.com/api/${terpName}`)
         const data = await response.json()
         console.log(data)
+
+        document.getElementById('terpName').innerText = data.terpName
+        document.getElementById('scent').innerText = data.scent
+        document.getElementById('benefits').innerText = data.benefits
+        document.getElementById('effects').innerText = data.effects
+        document.getElementById('nature').innerText = data.nature
+        document.getElementById('strains').innerText = data.strains
+        document.getElementById('image').src = data.terpImage
+        document.getElementById('caption').innerText = data.caption
     } catch(error) {
         console.log(error)
     }
