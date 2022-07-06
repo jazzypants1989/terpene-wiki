4fade2477970df1fcd2c833e47e9366161eda236
jazzypants1989 <jessepence@gmail.com>
2022-07-06 13:38:04 -0700

    beeboop

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 document.querySelector('button').addEventListener('click',apiRequest)
 
 async function apiRequest(){
-    const terpName = document.querySelector('input').value.toLowerCase()
+    const terpeneName = document.querySelector('input').value.toLowerCase()
     try{
-        const response = await fetch(`https://terpene-api-demo.herokuapp.com/api/${terpName}`)
+        const response = await fetch(`https://terpene-api-demo.herokuapp.com/api/${terpeneName}`)
         const data = await response.json()
         console.log(data)
 
         document.getElementById('terpName').innerText = data.terpName
         document.getElementById('scent').innerText = data.scent
-        document.getElementById('benefits').innerText = data.benefits
-        document.getElementById('effects').innerText = data.effects
+        document.getElementById('benefits').innerText = data.benefits.join(', ')
+        document.getElementById('effects').innerText = data.effects.join(', ')
         document.getElementById('nature').innerText = data.nature
-        document.getElementById('strains').innerText = data.strains
-        document.getElementById('image').src = data.terpImage
-        document.getElementById('caption').innerText = data.caption
+        document.getElementById('strains').innerText = data.strains.join(', ')
+        document.getElementById('image').src = data.image
+        document.getElementById('caption').innerText = data.terpName
     } catch(error) {
         console.log(error)
     }
